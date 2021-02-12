ea2abaab859ed0b0f993807ba7ec1a00b7d5c786
Anna Sokolova <sokolovaskorik@gmail.com>
2021-02-12 13:31:07 +0200

    Добавляет Корзину товаров и интерфейс

---
+++ js/view-cart-of-foods.js
@@ -0,0 +1,33 @@
+import cart from './cart-of-foods.js';
+
+const showCartBtn = document.querySelector('.js-show-cart');
+const addAppleCartBtn = document.querySelector('button[data-product="🍎"]');
+const addGrapeCartBtn = document.querySelector('button[data-product="🍇"]');
+const addLemonCartBtn = document.querySelector('button[data-product="🍋"]');
+const addStrawberryCartBtn = document.querySelector('button[data-product="🍓"]');
+
+
+
+showCartBtn.addEventListener('click', () => {
+  console.table(cart.getItems());
+});
+
+addAppleCartBtn.addEventListener(
+  'click',
+  cart.add.bind(cart, { name: '🍎', price: 50 }),
+);
+
+addGrapeCartBtn.addEventListener(
+  'click',
+  cart.add.bind(cart, { name: '🍇', price: 70 }),
+);
+
+addLemonCartBtn.addEventListener(
+  'click',
+  cart.add.bind(cart, { name: '🍋', price: 60 }),
+);
+
+addStrawberryCartBtn.addEventListener(
+  'click',
+  cart.add.bind(cart, { name: '🍓', price: 110 }),
+);
